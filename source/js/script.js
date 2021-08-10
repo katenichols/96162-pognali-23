77cb560f80e75b18303cd8da08e689cb554792c0
e.suslova <e.suslova@skbkontur.ru>
2021-08-10 17:37:13 +0500

    Работа с кнопкой открыть-закрыть модальное меню, подключение скрипта

---
+++ source/js/script.js
@@ -0,0 +1,8 @@
+let pageHeader = document.querySelector(".page-header");
+let logoWrapper = document.querySelector(".logo-wrapper");
+let modalMenu = document.querySelector(".modal-menu");
+let logoToggle = document.querySelector(".logo__toggle");
+
+pageHeader.classList.remove("page-header--nojs");
+logoWrapper.classList.remove("logo-wrapper--nojs");
+modalMenu.classList.remove("modal-menu--nojs");
