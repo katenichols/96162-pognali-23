dc8a8ed7e3bcba675cd199b66c7c1673b8d494fa
katenichols <kat-nik79@ya.ru>
2021-08-18 22:55:13 +0500

    Начаты стили блока тарифов

--- source/js/script.js
+++ source/js/script.js
@@ -3,10 +3,13 @@ let logoWrapper = document.querySelector(".logo-wrapper");
 let modalMenu = document.querySelector(".modal-menu");
 let logoToggle = document.querySelector(".logo__toggle");
 let logoToggleModal = document.querySelector(".logo__toggle-modal");
+let profile = document.querySelector(".profile__modal");
 
 pageHeader.classList.remove("page-header--nojs");
 logoWrapper.classList.remove("logo-wrapper--nojs");
 modalMenu.classList.remove("modal-menu--nojs");
+profile.classList.remove("profile__modal--nojs");
+profile.classList.add("profile__modal--closed");
 
 logoToggle.addEventListener("click", () => {
   if (modalMenu.classList.contains("modal-menu--opened")) {
