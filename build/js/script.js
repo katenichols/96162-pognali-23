b138b3a675d5c78fe0bcdba656807bbbffe358e0
katenichols <kat-nik79@ya.ru>
2021-08-20 00:14:15 +0500

    Закончены стили блока с тарифами

--- build/js/script.js
+++ build/js/script.js
@@ -3,15 +3,17 @@ let logoWrapper = document.querySelector(".logo-wrapper");
 let modalMenu = document.querySelector(".modal-menu");
 let logoToggle = document.querySelector(".logo__toggle");
 let logoToggleModal = document.querySelector(".logo__toggle-modal");
-let profile = document.querySelector(".profile__modal");
+let profileModal = document.querySelector(".profile__modal");
 let profileButton = document.querySelector(".profile__button");
+let profileButtonModal = document.querySelector(".profile__modal-button");
 
 pageHeader.classList.remove("page-header--nojs");
 logoWrapper.classList.remove("logo-wrapper--nojs");
 modalMenu.classList.remove("modal-menu--nojs");
-profile.classList.remove("profile__modal--nojs");
-profile.classList.add("profile__modal--closed");
+profileModal.classList.remove("profile__modal--nojs");
+profileModal.classList.add("profile__modal--closed");
 profileButton.classList.remove("profile__button--nojs");
+profileButtonModal.classList.remove("profile__modal-button--nojs");
 
 logoToggle.addEventListener("click", () => {
   if (modalMenu.classList.contains("modal-menu--opened")) {
@@ -27,4 +29,12 @@ logoToggleModal.addEventListener("click", () => {
   } else {
     modalMenu.classList.add("modal-menu--opened");
   }
+});
+
+profileButton.addEventListener("click", () => {
+  profileModal.classList.remove("profile__modal--closed");
+});
+
+profileButtonModal.addEventListener("click", () => {
+  profileModal.classList.add("profile__modal--closed");
 });
