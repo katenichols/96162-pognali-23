630bb91fc37429bd13d1805fcb731e1b75b1384c
e.suslova <e.suslova@skbkontur.ru>
2021-09-01 18:30:06 +0500

    Начаты стили декоративки catalog

---
+++ source/js/script-catalog.js
@@ -0,0 +1,26 @@
+const pageHeader = document.querySelector(".page-header");
+const logoWrapper = document.querySelector(".logo-wrapper");
+const modalMenu = document.querySelector(".modal-menu");
+const logoToggle = document.querySelector(".logo__toggle");
+const logoToggleModal = document.querySelector(".logo__toggle-modal");
+
+pageHeader.classList.remove("page-header--nojs");
+logoWrapper.classList.remove("logo-wrapper--nojs");
+modalMenu.classList.remove("modal-menu--nojs");
+logoToggleModal.classList.remove("logo__toggle-modal--nojs");
+
+logoToggle.addEventListener("click", () => {
+  if (modalMenu.classList.contains("modal-menu--opened")) {
+    modalMenu.classList.remove("modal-menu--opened");
+  } else {
+    modalMenu.classList.add("modal-menu--opened");
+  }
+});
+
+logoToggleModal.addEventListener("click", () => {
+  if (modalMenu.classList.contains("modal-menu--opened")) {
+    modalMenu.classList.remove("modal-menu--opened");
+  } else {
+    modalMenu.classList.add("modal-menu--opened");
+  }
+});
